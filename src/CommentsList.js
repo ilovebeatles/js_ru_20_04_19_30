be13b32102c4b5522cbf9717480599f6c9ddff57
Pavel Shabashov <forcu2mblog@gmail.com>
2017-05-07 16:48:40 +0400

    add requested changes

--- src/CommentsList.js
+++ src/CommentsList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Comment from './Comment';
 
 export default function CommentsList({comments}) {
+	if (!comments || !comments.length) return null;
 	const elements = comments.map(comment => <li key={comment.id}><Comment comment={comment}/> </li>);
 	return (
 		<div className="comments">
